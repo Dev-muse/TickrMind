1250867f790029673b3b713e50b6e8cde6601f05
Rahman <museabdirahman@gmail.com>
2025-10-01 14:04:13 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div className={'flex justify-center items-center h-screen '}>
+
+          <Button>Hell</Button>
+        </div>
+    )
+}
+export default Page
